1dcef75f603fb15fef55ebdb70d99d28929dd085
Matias <matiasdecarli@gmail.com>
2015-01-02 05:37:42 +0000

    basic func

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,28 +8,48 @@
 
 'use strict';
 
+var fs = require('fs');
+var fileTarget = '.target'
+
 module.exports = function(grunt) {
 
 // Project configuration.
   grunt.initConfig({
     // Configuration to be run (and then tested).
     'req_cleaner': {
-        options: {
-        }, 
-        build: {
-            src: [                    
-                'test_files/*.js'                
-            ],            
-            expand: true,
-            filter: 'isFile',
-        }
+      options: {
+      }, 
+      build: {
+          src: [                    
+              'test_files/*.js'                
+          ],            
+          expand: true,
+          filter: 'isFile',
       }
+    },
+    prompt: {
+      target: {
+        options: {
+          questions: [
+            {
+              config: 'config.name',
+              type: 'checkbox',
+              message: 'Select requires to delete...', 
+              default: 'value',
+              choices: function(){
+                  return fs.readFileSync(fileTarget, 'UTF-8').toString().split("\n");                  
+              }
+            }
+          ]
+        }
+      },
+    }    
   });
 
   // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
+  grunt.loadNpmTasks('grunt-prompt');
 
-  // By default, lint and run all tests.
+  // By default, lint and run all tests.  
   grunt.registerTask('default', ['req_cleaner']);
-
 };
--- tasks/req_cleaner.js
+++ tasks/req_cleaner.js
@@ -14,16 +14,57 @@ var async = require('async'),
 module.exports = function(grunt) {
 
   grunt.registerMultiTask('req_cleaner', 'A grunt task to clean all unused require()', function() {
-      var done = this.async();  
-      var files = this.files;                
+      var done = this.async();
 
-      async.each(files, function (item){
-        processFile(fs.readFileSync(item.src.toString(), 'UTF-8'))
+      async.each(this.files, function (item){
+        processFile(fs.readFileSync(item.src.toString(), 'UTF-8').toString().split("\n"), item.src.toString());
       }, done());
   });
 
-  function processFile(contents){
-    //magic to come
-  }
+  function processFile(contents, fileName){
+    var requires = [];
+    var lineNumber = 0;
+    var haveRequires = new RegExp(/^[^\/].*require\('/);                  //check if there a require() in line
+    var filterRequire = new RegExp(/var(.*?)(.*?)(?==require\('|$)/i);    //leaves only the var name
 
+    //1rst run to discover requires
+    contents.forEach(function(line){
+      if (haveRequires.test(line.replace(/ /g,''))){
+        requires.push({
+          require: filterRequire.exec(line.replace(/ /g,'').toString())[2],
+          file: fileName,
+          line: lineNumber++,
+          exists: false
+        });
+       }
+    });
+
+    var regexRequire = "[^a-zA-Z0-9$_]R[^a-zA-Z0-9$_]";  //look for the require()
+    if (requires.length>0){
+      //2nd run to eliminate unused 
+      for(var i=0; i<requires.length;i++){
+        var containsRequire = new RegExp(regexRequire.replace("R",requires[i].require));
+        for(var y=0;y<contents.length;y++){
+          if ( (contents[y]) && (containsRequire.test(contents[y])) && (y>0) && (requires[i].line != y-1) ){
+              requires[i].exists = true;
+              break;
+          }
+        }
+      }
+    }
+
+    var waste = [];
+    requires.forEach(function(item){
+      if (item.exists==false){   
+        waste.push(item);
+      }
+    });
+
+    if (waste.length>0){
+      grunt.log.error('file', fileName, 'has', waste.length, 'unused requires');
+    }
+    else{
+      console.log('file', fileName, 'has no unused requires');
+    }
+  }
 };
